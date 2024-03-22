a82ec848864df8e3dbb869cd12d01fded6347106
Damian <damianmarti@gmail.com>
2024-03-22 09:52:23 -0300

    Add receive modal

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -4,6 +4,7 @@ import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { History } from "~~/components/burnerwallet/History";
 import { SendETHModal } from "~~/components/burnerwallet/SendETHModal";
+import { AddressQRCodeModal } from "~~/components/scaffold-eth/RainbowKitCustomConnectButton/AddressQRCodeModal";
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
@@ -12,6 +13,7 @@ const Home: NextPage = () => {
     <div className="flex flex-col gap-2 text-center m-auto overflow-x-hidden">
       {connectedAddress && <History address={connectedAddress} />}
       <SendETHModal modalId="send-eth-modal" />
+      {connectedAddress && <AddressQRCodeModal modalId="qrcode-modal" address={connectedAddress} />}
     </div>
   );
 };
