927312d7ef330dadc7ca51ff8260089aa938edf3
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-06 09:14:48 +0800

    Limit Desktop Container (#6)

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -21,31 +21,29 @@ export const Header = () => {
   const { chain } = useNetwork();
 
   return (
-    <div className="max-w-xl mx-auto">
-      <div className="p-6 bg-pink-200">
-        <div className="flex justify-between items-center mb-6">
-          <Cog6ToothIcon className="w-6" />
-          <NetworksDropdown
-            onChange={option => switchNetwork?.(option.value)}
-            value={chain ? chain.id : networks[0].id}
-          />
-        </div>
-        <div>
-          <Address address={connectedAddress} disableAddressLink size="xl" format="short" />
-        </div>
-        <div className="mt-4 flex justify-center">
-          <Balance className="text-2xl" address={connectedAddress} />
-        </div>
-        <div className="flex items-center justify-center gap-6 mt-6">
-          <ReceiveDrawer address={connectedAddress} />
-          <label
-            htmlFor="send-eth-modal"
-            className="flex items-center bg-white text-custom-black dark:text-black rounded-full px-4 py-2"
-          >
-            <PaperAirplaneIcon className="w-8 mr-2" />
-            <span className="font-bold">Send</span>
-          </label>
-        </div>
+    <div className="p-6 bg-pink-200">
+      <div className="flex justify-between items-center mb-6">
+        <Cog6ToothIcon className="w-6" />
+        <NetworksDropdown
+          onChange={option => switchNetwork?.(option.value)}
+          value={chain ? chain.id : networks[0].id}
+        />
+      </div>
+      <div>
+        <Address address={connectedAddress} disableAddressLink size="xl" format="short" />
+      </div>
+      <div className="mt-4 flex justify-center">
+        <Balance className="text-2xl" address={connectedAddress} />
+      </div>
+      <div className="flex items-center justify-center gap-6 mt-6">
+        <ReceiveDrawer address={connectedAddress} />
+        <label
+          htmlFor="send-eth-modal"
+          className="flex items-center bg-white text-custom-black dark:text-black rounded-full px-4 py-2"
+        >
+          <PaperAirplaneIcon className="w-8 mr-2" />
+          <span className="font-bold">Send</span>
+        </label>
       </div>
     </div>
   );
