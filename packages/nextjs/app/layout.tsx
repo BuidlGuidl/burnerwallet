27503cd28fc9c947bf1eaccecf3dbea2d6cc7dac
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-09 20:40:07 +0800

    refactor: adjust favicon and various styles

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
     description,
   },
   icons: {
-    icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
+    icon: [{ url: "/favicon.ico", sizes: "32x32", type: "image/x-icon" }],
   },
 };
 
--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
   return (
     <footer>
       <button
-        className="fixed bg-primary inline-block p-4 rounded-full bottom-0 mb-8 left-0 right-0 w-[72px] m-auto"
+        className="fixed bg-primary inline-block p-4 rounded-full bottom-0 mb-6 left-0 right-0 w-[72px] m-auto"
         onClick={() => setIsQrReaderOpen(true)}
       >
         <ScanIcon width="2.5rem" height="2.5rem" />
