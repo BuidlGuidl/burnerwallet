7cde58ac67a983a3fb3459ed38ad6ce8bc81a30e
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-09 11:16:15 +0800

    move usd price into settings drawer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -1,45 +1,14 @@
-import Link from "next/link";
 import ScanIcon from "../icons/ScanIcon";
-import { hardhat } from "viem/chains";
-import { CurrencyDollarIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Faucet } from "~~/components/scaffold-eth";
-import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
 import { useGlobalState } from "~~/services/store/store";
 
 /**
  * Site footer
  */
 export const Footer = () => {
-  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
   const setIsQrReaderOpen = useGlobalState(state => state.setIsQrReaderOpen);
-  const { targetNetwork } = useTargetNetwork();
-  const isLocalNetwork = targetNetwork.id === hardhat.id;
 
   return (
     <footer>
-      <div>
-        <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
-          <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
-            {nativeCurrencyPrice > 0 && (
-              <div>
-                <div className="btn btn-primary btn-sm font-normal gap-1 cursor-auto">
-                  <CurrencyDollarIcon className="h-4 w-4" />
-                  <span>{nativeCurrencyPrice}</span>
-                </div>
-              </div>
-            )}
-            {isLocalNetwork && (
-              <>
-                <Faucet />
-                <Link href="/blockexplorer" passHref className="btn btn-primary btn-sm font-normal gap-1">
-                  <MagnifyingGlassIcon className="h-4 w-4" />
-                  <span>Block Explorer</span>
-                </Link>
-              </>
-            )}
-          </div>
-        </div>
-      </div>
       <button
         className="fixed bg-primary inline-block p-4 rounded-full bottom-0 mb-8 left-0 right-0 w-[72px] m-auto"
         onClick={() => setIsQrReaderOpen(true)}
--- packages/nextjs/components/burnerwallet/SettingsDrawer.tsx
+++ packages/nextjs/components/burnerwallet/SettingsDrawer.tsx
@@ -1,4 +1,6 @@
-import { ChevronLeftIcon, Cog6ToothIcon, HeartIcon } from "@heroicons/react/24/outline";
+"use client";
+
+import { ChevronLeftIcon, Cog6ToothIcon, CurrencyDollarIcon, HeartIcon } from "@heroicons/react/24/outline";
 import {
   Drawer,
   DrawerClose,
@@ -9,8 +11,11 @@ import {
   DrawerTrigger,
 } from "~~/components/Drawer";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
+import { useGlobalState } from "~~/services/store/store";
 
 export const SettingsDrawer = () => {
+  const nativeCurrencyPrice = useGlobalState(state => state.nativeCurrencyPrice);
+
   return (
     <Drawer direction="left">
       <DrawerTrigger className="btn btn-sm btn-ghost">
@@ -24,7 +29,15 @@ export const SettingsDrawer = () => {
           <DrawerTitle className="text-xl md:text-2xl">Settings</DrawerTitle>
         </DrawerHeader>
         <div className="flex flex-col items-center gap-4 mb-4 px-4"></div>
-        <DrawerFooter>
+        <DrawerFooter className="pb-6 gap-4">
+          {nativeCurrencyPrice > 0 && (
+            <div className="mx-auto">
+              <div className="btn btn-primary btn-sm font-normal gap-1 cursor-auto">
+                <CurrencyDollarIcon className="h-4 w-4" />
+                <span>{nativeCurrencyPrice}</span>
+              </div>
+            </div>
+          )}
           <div className="flex justify-center items-center gap-2">
             <p className="m-0 text-center">
               Built with <HeartIcon className="inline-block h-4 w-4" /> at
