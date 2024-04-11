05c0968d9bf5388c865cbca013436873fe9bf18f
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-11 09:15:35 +0800

    style: Adding Onest Font (#18)
    * adding onest font
    
    * remove tight tracking on DrawerTitle

--- packages/nextjs/components/Drawer.tsx
+++ packages/nextjs/components/Drawer.tsx
@@ -57,11 +57,7 @@ const DrawerTitle = React.forwardRef<
   React.ElementRef<typeof DrawerPrimitive.Title>,
   React.ComponentPropsWithoutRef<typeof DrawerPrimitive.Title>
 >(({ className, ...props }, ref) => (
-  <DrawerPrimitive.Title
-    ref={ref}
-    className={cn("text-lg font-semibold leading-none tracking-tight", className)}
-    {...props}
-  />
+  <DrawerPrimitive.Title ref={ref} className={cn("text-lg font-semibold leading-none", className)} {...props} />
 ));
 DrawerTitle.displayName = DrawerPrimitive.Title.displayName;
 
