4c36d85708eea831c43c6a706ab8b86ef2bc3e2e
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-04-20 15:26:41 +0800

    trigger updateHistory after send drawer transaction success

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -8,11 +8,11 @@ import { useGetHistory } from "~~/hooks/useGetHistory";
 
 const Home: NextPage = () => {
   const { address: connectedAddress = "" } = useAccount();
-  const { chainId, isLoading, history } = useGetHistory({ address: connectedAddress });
+  const { chainId, isLoading, history, updateHistory } = useGetHistory({ address: connectedAddress });
 
   return (
     <>
-      <Header />
+      <Header updateHistory={updateHistory} />
       <main>
         <div className="max-w-xl mx-auto">
           <section className="px-6 pb-28 pt-2 divide-y">
--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -16,7 +16,7 @@ const networks = getTargetNetworks();
 /**
  * Site header
  */
-export const Header = () => {
+export const Header = ({ updateHistory }: { updateHistory: () => void }) => {
   useAutoConnect();
 
   const setChainId = useLocalStorage<number>(SCAFFOLD_CHAIN_ID_STORAGE_KEY, networks[0].id)[1];
@@ -57,7 +57,7 @@ export const Header = () => {
         </div>
         <div className="flex items-center justify-center gap-6 mt-6">
           <ReceiveDrawer address={connectedAddress} />
-          <SendDrawer address={connectedAddress} />
+          <SendDrawer address={connectedAddress} updateHistory={updateHistory} />
         </div>
       </div>
     </div>
