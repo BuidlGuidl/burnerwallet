85fa5f7540007aa6b56221b82eba634621d220de
Damian <damianmarti@gmail.com>
2024-05-16 16:46:23 -0300

    Confirmation transaction on modal

--- packages/nextjs/components/burnerwallet/WalletConnectDrawer.tsx
+++ packages/nextjs/components/burnerwallet/WalletConnectDrawer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { SessionTypes } from "@walletconnect/types";
 import { parseUri } from "@walletconnect/utils";
 import { buildApprovedNamespaces, getSdkError } from "@walletconnect/utils";
@@ -29,6 +29,10 @@ export const WalletConnectDrawer = () => {
 
   const [loading, setLoading] = useState(false);
   const [session, setSession] = useState<SessionTypes.Struct | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [confirmationData, setConfirmationData] = useState<any>({});
+  const [confirmationTitle, setConfirmationTitle] = useState("");
+  const [confirmationInfo, setConfirmationInfo] = useState<ReactNode>();
 
   const setChainId = useLocalStorage<number>(SCAFFOLD_CHAIN_ID_STORAGE_KEY, networks[0].id)[1];
 
@@ -39,6 +43,15 @@ export const WalletConnectDrawer = () => {
     },
   });
 
+  function getAccount() {
+    let currentSk: Hex = "0x";
+    if (typeof window != "undefined" && window != null) {
+      currentSk = (window?.localStorage?.getItem?.(burnerStorageKey)?.replaceAll('"', "") ?? "0x") as Hex;
+    }
+    const account = privateKeyToAccount(currentSk);
+    return account;
+  }
+
   async function onConnect(uri: string) {
     const { topic: pairingTopic } = parseUri(uri);
 
@@ -48,15 +61,6 @@ export const WalletConnectDrawer = () => {
         web3wallet.core.pairing.events.removeListener("pairing_expire", pairingExpiredListener);
       }
     };
-
-    function getAccount() {
-      let currentSk: Hex = "0x";
-      if (typeof window != "undefined" && window != null) {
-        currentSk = (window?.localStorage?.getItem?.(burnerStorageKey)?.replaceAll('"', "") ?? "0x") as Hex;
-      }
-      const account = privateKeyToAccount(currentSk);
-      return account;
-    }
 
     async function onSessionProposal({ id, params }: Web3WalletTypes.SessionProposal) {
       try {
@@ -105,8 +109,6 @@ export const WalletConnectDrawer = () => {
       const { chainId, request } = params;
       const requestParamsMessage = request.params[0];
 
-      const account: PrivateKeyAccount = getAccount();
-
       if (!chain) {
         return await web3wallet.respondSessionRequest({
           topic,
@@ -149,106 +151,57 @@ export const WalletConnectDrawer = () => {
         }
       }
 
-      const wallet = createWalletClient({
-        chain: chainFromRequest,
-        account: account,
-        transport: http(),
-      });
-
       switch (request.method) {
         case EIP155_SIGNING_METHODS.PERSONAL_SIGN:
         case EIP155_SIGNING_METHODS.ETH_SIGN:
-          try {
-            const message = hexToString(requestParamsMessage);
-            // TODO: improve this
-            if (confirm("Do you want to sign this message? Message: " + message)) {
-              const signedMessage = await wallet.signMessage({ message });
-              const response = { id, result: signedMessage, jsonrpc: "2.0" };
-
-              return await web3wallet.respondSessionRequest({ topic, response });
-            } else {
-              throw new Error("User rejected signature");
-            }
-          } catch (error: any) {
-            console.error(error);
-            return await web3wallet.respondSessionRequest({
-              topic,
-              response: errorResponse(id, error.message),
-            });
-          }
+          const messageToSign = hexToString(requestParamsMessage);
+          setConfirmationData({
+            id,
+            topic,
+            chain: chainFromRequest,
+            method: request.method,
+            data: messageToSign,
+          });
+          setIsModalOpen(true);
+          return;
         case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA:
         case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V3:
         case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V4:
-          try {
-            let data = request.params.filter((p: any) => !isAddress(p))[0];
+          let data = request.params.filter((p: any) => !isAddress(p))[0];
 
-            // TODO: improve this
-            if (confirm("Do you want to sign this message? Message: " + data)) {
-              if (typeof data === "string") {
-                data = JSON.parse(data);
-              }
-
-              const { domain, types, message, primaryType } = data;
-
-              const signedData = await wallet.signTypedData({ domain, types, message, primaryType });
-              const response = { id, result: signedData, jsonrpc: "2.0" };
-
-              return await web3wallet.respondSessionRequest({ topic, response });
-            } else {
-              throw new Error("User rejected signature");
-            }
-          } catch (error: any) {
-            console.error(error);
-            return await web3wallet.respondSessionRequest({
-              topic,
-              response: errorResponse(id, error.message),
-            });
+          if (typeof data === "string") {
+            data = JSON.parse(data);
           }
+          setConfirmationData({
+            id,
+            topic,
+            chain: chainFromRequest,
+            method: request.method,
+            data: data,
+          });
+          setIsModalOpen(true);
+          return;
         case EIP155_SIGNING_METHODS.ETH_SEND_TRANSACTION:
-          try {
-            // TODO: improve this
-            if (confirm("Do you want to send this transaction? Transaction: " + requestParamsMessage)) {
-              if (typeof requestParamsMessage.value === "string") {
-                if (requestParamsMessage.value.endsWith("n")) {
-                  requestParamsMessage.value = BigInt(requestParamsMessage.value.slice(0, -1));
-                } else {
-                  requestParamsMessage.value = hexToBigInt(requestParamsMessage.value);
-                }
-              }
-
-              const hash = await wallet.sendTransaction(requestParamsMessage);
-              const response = { id, result: hash, jsonrpc: "2.0" };
-
-              return await web3wallet.respondSessionRequest({ topic, response });
-            } else {
-              throw new Error("User rejected transaction");
-            }
-          } catch (error: any) {
-            console.error(error);
-            return await web3wallet.respondSessionRequest({
-              topic,
-              response: errorResponse(id, error.message),
-            });
-          }
-        // TODO: test
         case EIP155_SIGNING_METHODS.ETH_SIGN_TRANSACTION:
-          try {
-            // TODO: improve this
-            if (confirm("Do you want to sign this transaction? Transaction: " + requestParamsMessage)) {
-              const signature = await wallet.signTransaction(requestParamsMessage);
-              const response = { id, result: signature, jsonrpc: "2.0" };
-
-              return await web3wallet.respondSessionRequest({ topic, response });
-            } else {
-              throw new Error("User rejected sign transaction");
+          const fieldsToCheck = ["value", "gas"];
+          for (const field of fieldsToCheck) {
+            if (typeof requestParamsMessage[field] === "string") {
+              if (requestParamsMessage[field].endsWith("n")) {
+                requestParamsMessage[field] = BigInt(requestParamsMessage[field].slice(0, -1));
+              } else {
+                requestParamsMessage[field] = hexToBigInt(requestParamsMessage[field]);
+              }
             }
-          } catch (error: any) {
-            console.error(error);
-            return await web3wallet.respondSessionRequest({
-              topic,
-              response: errorResponse(id, error.message),
-            });
           }
+          setConfirmationData({
+            id,
+            topic,
+            chain: chainFromRequest,
+            method: request.method,
+            data: requestParamsMessage,
+          });
+          setIsModalOpen(true);
+          return;
         default:
           console.error("Invalid Method");
           return await web3wallet.respondSessionRequest({
@@ -293,34 +246,182 @@ export const WalletConnectDrawer = () => {
     }
   }
 
+  async function onConfirmTransaction() {
+    try {
+      setLoading(true);
+      const account: PrivateKeyAccount = getAccount();
+      const wallet = createWalletClient({
+        chain: confirmationData.chain,
+        account: account,
+        transport: http(),
+      });
+      switch (confirmationData.method) {
+        case EIP155_SIGNING_METHODS.PERSONAL_SIGN:
+        case EIP155_SIGNING_METHODS.ETH_SIGN:
+          const signedMessage = await wallet.signMessage({ message: confirmationData.data });
+          const responseSign = { id: confirmationData.id, result: signedMessage, jsonrpc: "2.0" };
+
+          await web3wallet.respondSessionRequest({ topic: confirmationData.topic, response: responseSign });
+          break;
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA:
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V3:
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V4:
+          const { domain, types, message, primaryType } = confirmationData.data;
+
+          const signedData = await wallet.signTypedData({ domain, types, message, primaryType });
+          const responseSignData = { id: confirmationData.id, result: signedData, jsonrpc: "2.0" };
+
+          await web3wallet.respondSessionRequest({ topic: confirmationData.topic, response: responseSignData });
+          break;
+        case EIP155_SIGNING_METHODS.ETH_SEND_TRANSACTION:
+          const hash = await wallet.sendTransaction(confirmationData.data);
+          const responseSendTransaction = { id: confirmationData.id, result: hash, jsonrpc: "2.0" };
+
+          await web3wallet.respondSessionRequest({ topic: confirmationData.topic, response: responseSendTransaction });
+          break;
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TRANSACTION:
+          const signature = await wallet.signTransaction(confirmationData.data);
+          const responseSignTransaction = { id: confirmationData.id, result: signature, jsonrpc: "2.0" };
+
+          await web3wallet.respondSessionRequest({ topic: confirmationData.topic, response: responseSignTransaction });
+          break;
+      }
+    } catch (error: any) {
+      notification.error((error as Error).message);
+      console.error(error);
+      setIsModalOpen(false);
+      return await web3wallet.respondSessionRequest({
+        topic: confirmationData.topic,
+        response: errorResponse(confirmationData.id, error.message),
+      });
+    } finally {
+      setLoading(false);
+      setIsModalOpen(false);
+    }
+  }
+
+  async function onRejectTransaction() {
+    setLoading(true);
+    let message = "User rejected ";
+    if (confirmationData.method === EIP155_SIGNING_METHODS.ETH_SEND_TRANSACTION) {
+      message += "send transaction";
+    }
+    if (confirmationData.method === EIP155_SIGNING_METHODS.ETH_SIGN_TRANSACTION) {
+      message += "sign transaction";
+    }
+    await web3wallet.respondSessionRequest({
+      topic: confirmationData.topic,
+      response: errorResponse(confirmationData.id, message),
+    });
+    setLoading(false);
+    setIsModalOpen(false);
+  }
+
+  useEffect(() => {
+    if (!confirmationData || !confirmationData.method) {
+      setConfirmationTitle("");
+    } else {
+      switch (confirmationData.method) {
+        case EIP155_SIGNING_METHODS.PERSONAL_SIGN:
+        case EIP155_SIGNING_METHODS.ETH_SIGN:
+          setConfirmationTitle("Do you want to sign this message?");
+          setConfirmationInfo(
+            <div>
+              <dt>Message:</dt>
+              <dd>{confirmationData.data}</dd>
+            </div>,
+          );
+          break;
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA:
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V3:
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TYPED_DATA_V4:
+          setConfirmationTitle("Do you want to sign this message?");
+          setConfirmationInfo(
+            <div>
+              <dt>Message:</dt>
+              <dd>{JSON.stringify(confirmationData.data.message)}</dd>
+            </div>,
+          );
+          break;
+        case EIP155_SIGNING_METHODS.ETH_SEND_TRANSACTION:
+        case EIP155_SIGNING_METHODS.ETH_SIGN_TRANSACTION:
+          let action = "send";
+          if (confirmationData.method === EIP155_SIGNING_METHODS.ETH_SIGN_TRANSACTION) {
+            action = "sign";
+          }
+          setConfirmationTitle(`Do you want to ${action} this transaction?`);
+          setConfirmationInfo(
+            <div>
+              <p>
+                <dt className="inline mr-2">From:</dt>
+                <dd className="inline">{confirmationData.data.from}</dd>
+              </p>
+              <p>
+                <dt className="inline mr-2">To:</dt>
+                <dd className="inline">{confirmationData.data.to}</dd>
+              </p>
+              <p>
+                <dt className="inline mr-2">Value:</dt>
+                <dd className="inline">{confirmationData.data.value?.toString()}</dd>
+              </p>
+              <p>
+                <dt className="inline mr-2">Data:</dt>
+                <dd className="inline">{confirmationData.data.data}</dd>
+              </p>
+            </div>,
+          );
+          break;
+        default:
+          setConfirmationTitle("");
+          break;
+      }
+    }
+  }, [confirmationData.method]);
+
   return (
-    <Drawer open={isWalletConnectOpen} onOpenChange={setIsWalletConnectOpen}>
-      <DrawerContent>
-        <DrawerLine />
-        <DrawerHeader>
-          <DrawerTitle className="mt-1 text-2xl">WalletConnect</DrawerTitle>
-        </DrawerHeader>
-        <div>
-          <div className="max-w-lg mx-auto mb-8 text-center">
-            {session ? (
-              <div>
-                <div>Connected to Dapp</div>
-                <button className="btn btn-neutral bg-white/50" onClick={disconnect}>
-                  Disconnect
+    <>
+      <Drawer open={isWalletConnectOpen} onOpenChange={setIsWalletConnectOpen}>
+        <DrawerContent>
+          <DrawerLine />
+          <DrawerHeader>
+            <DrawerTitle className="mt-1 text-2xl">WalletConnect</DrawerTitle>
+          </DrawerHeader>
+          <div>
+            <div className="max-w-lg mx-auto mb-8 text-center">
+              {session ? (
+                <div>
+                  <div>Connected to Dapp</div>
+                  <button className="btn btn-neutral bg-white/50" onClick={disconnect}>
+                    Disconnect
+                  </button>
+                </div>
+              ) : (
+                <button
+                  disabled={!walletConnectUid || loading}
+                  className="btn btn-neutral bg-white/50"
+                  onClick={() => onConnect(walletConnectUid)}
+                >
+                  {loading ? "Loading..." : "Connect to Dapp"}
                 </button>
-              </div>
-            ) : (
-              <button
-                disabled={!walletConnectUid || loading}
-                className="btn btn-neutral bg-white/50"
-                onClick={() => onConnect(walletConnectUid)}
-              >
-                {loading ? "Loading..." : "Connect to Dapp"}
-              </button>
-            )}
+              )}
+            </div>
+          </div>
+        </DrawerContent>
+      </Drawer>
+      <dialog id="wallet-connect-confirmation-modal" className="modal overflow-y-auto" open={isModalOpen}>
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">{confirmationTitle}</h3>
+          <p className="py-4">{confirmationInfo}</p>
+          <div className="modal-action">
+            <button className="btn" disabled={loading} onClick={onRejectTransaction}>
+              Reject
+            </button>
+            <button className="btn" disabled={loading} onClick={onConfirmTransaction}>
+              Confirm
+            </button>
           </div>
         </div>
-      </DrawerContent>
-    </Drawer>
+      </dialog>
+    </>
   );
 };
