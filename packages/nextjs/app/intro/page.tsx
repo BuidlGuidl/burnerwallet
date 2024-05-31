82f58beb1effdccd662b733fcc098c185be639fc
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-05-31 12:22:26 +0800

    add images to landing page

--- packages/nextjs/app/intro/page.tsx
+++ packages/nextjs/app/intro/page.tsx
@@ -1,5 +1,11 @@
 import { cookies } from "next/headers";
+import Image from "next/image";
 import { redirect } from "next/navigation";
+import ScreenOne from "./img/screen-1.png";
+import ScreenTwo from "./img/screen-2.png";
+import ScreenThree from "./img/screen-3.png";
+import ScreenFour from "./img/screen-4.png";
+import ScreenFive from "./img/screen-5.png";
 import { DocumentArrowUpIcon, PaintBrushIcon, WindowIcon } from "@heroicons/react/20/solid";
 
 async function setIntroCookie() {
@@ -32,12 +38,22 @@ const features = [
   },
 ];
 
+const GenerateButton = () => {
+  return (
+    <form action={setIntroCookie}>
+      <button className="btn btn-primary btn-lg px-10" type="submit">
+        Generate Burner Wallet
+      </button>
+    </form>
+  );
+};
+
 export default function IntroPage() {
   return (
     <main className="bg-base-200 overflow-hidden">
       <div className="relative isolate">
         <div
-          className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+          className="absolute right-1/2 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
           aria-hidden="true"
         >
           <div
@@ -49,7 +65,7 @@ export default function IntroPage() {
           />
         </div>
         <div className="overflow-hidden">
-          <div className="mx-auto max-w-7xl px-6 pb-32 pt-36 sm:pt-60 lg:px-8 lg:pt-32">
+          <div className="mx-auto max-w-7xl px-6 pb-32 pt-12 sm:pt-24 lg:px-8 lg:pt-32">
             <div className="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
               <div className="relative w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                 <h1 className="text-4xl font-bold tracking-tight sm:text-6xl">Burner Wallet</h1>
@@ -64,18 +80,14 @@ export default function IntroPage() {
                 </p>
 
                 <div className="mt-10 flex items-center gap-x-6">
-                  <form action={setIntroCookie}>
-                    <button className="btn btn-primary btn-lg" type="submit">
-                      Generate Burner Wallet
-                    </button>
-                  </form>
+                  <GenerateButton />
                 </div>
               </div>
               <div className="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                 <div className="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                   <div className="relative">
-                    <img
-                      src="https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    <Image
+                      src={ScreenOne}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
@@ -84,16 +96,16 @@ export default function IntroPage() {
                 </div>
                 <div className="mr-auto w-44 flex-none space-y-8 sm:mr-0 sm:pt-52 lg:pt-36">
                   <div className="relative">
-                    <img
-                      src="https://images.unsplash.com/photo-1485217988980-11786ced9454?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    <Image
+                      src={ScreenTwo}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                   </div>
                   <div className="relative">
-                    <img
-                      src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=396&h=528&q=80"
+                    <Image
+                      src={ScreenThree}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
@@ -102,16 +114,16 @@ export default function IntroPage() {
                 </div>
                 <div className="w-44 flex-none space-y-8 pt-32 sm:pt-0">
                   <div className="relative">
-                    <img
-                      src="https://images.unsplash.com/photo-1670272504528-790c24957dda?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=left&w=400&h=528&q=80"
+                    <Image
+                      src={ScreenFour}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
                     <div className="pointer-events-none absolute inset-0 rounded-xl ring-1 ring-inset ring-gray-900/10" />
                   </div>
                   <div className="relative">
-                    <img
-                      src="https://images.unsplash.com/photo-1670272505284-8faba1c31f7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&h=528&q=80"
+                    <Image
+                      src={ScreenFive}
                       alt=""
                       className="aspect-[2/3] w-full rounded-xl bg-gray-900/5 object-cover shadow-lg"
                     />
@@ -141,19 +153,15 @@ export default function IntroPage() {
                     <feature.icon className="h-5 w-5 flex-none text-accent" aria-hidden="true" />
                     {feature.name}
                   </dt>
-                  <dd className="mt-4 flex flex-auto flex-col text-base leading-7">
+                  <dd className="lg:mt-2 flex flex-auto flex-col text-base leading-7">
                     <p className="flex-auto">{feature.description}</p>
                   </dd>
                 </div>
               ))}
             </dl>
           </div>
-          <div className="mt-8 text-center">
-            <form action={setIntroCookie}>
-              <button className="btn btn-primary btn-lg" type="submit">
-                Generate Burner Wallet
-              </button>
-            </form>
+          <div className="mt-12 text-center">
+            <GenerateButton />
           </div>
         </div>
       </div>
