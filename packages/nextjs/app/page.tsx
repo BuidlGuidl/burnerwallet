b398be0219d1311fa591f8b55466e7ff8ca1b8db
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-06-01 11:45:48 +0800

    remove intro page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,15 +1,10 @@
 import { cookies } from "next/headers";
-// import { redirect } from "next/navigation";
 import type { NextPage } from "next";
 import Homepage from "~~/components/Homepage";
 
 const Home: NextPage = () => {
   const cookieStore = cookies();
   const hasSeenIntro = cookieStore.has("hasSeenIntro");
-
-  // if (!hasSeenIntro) {
-  //   redirect("/intro");
-  // }
 
   return <Homepage hasSeenIntro={hasSeenIntro} />;
 };
