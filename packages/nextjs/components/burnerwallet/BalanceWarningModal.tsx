5d7a6984388e1bdff89483c1a2d3cb9df00b35df
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-06-03 13:07:04 +0800

    use hero icon for balance warning

--- packages/nextjs/components/burnerwallet/BalanceWarningModal.tsx
+++ packages/nextjs/components/burnerwallet/BalanceWarningModal.tsx
@@ -3,6 +3,7 @@
 import { useCallback, useEffect, useState } from "react";
 import { useLocalStorage } from "usehooks-ts";
 import { useAccount } from "wagmi";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { useAccountBalance } from "~~/hooks/scaffold-eth";
 
 const BURNER_WALLET_HIDE_BALANCE_STORAGE_KEY = "scaffoldEth2.hideBalanceWarning";
@@ -11,9 +12,7 @@ export const BalanceWarningModal = () => {
   const [hideBalanceWarning, setHideBalanceWarning] = useLocalStorage<boolean>(
     BURNER_WALLET_HIDE_BALANCE_STORAGE_KEY,
     false,
-    {
-      initializeWithValue: false,
-    },
+    { initializeWithValue: false },
   );
 
   const { address } = useAccount();
@@ -55,19 +54,7 @@ export const BalanceWarningModal = () => {
       <div className="modal" role="dialog">
         <div className="modal-box">
           <div role="alert" className="alert alert-warning">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="stroke-current shrink-0 h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-              />
-            </svg>
+            <ExclamationTriangleIcon className="w-6 h-6" />
             <span>
               Warning: Your Burner Wallet Has Funds! Be sure to back-up your private key to avoid loss of funds.
             </span>
